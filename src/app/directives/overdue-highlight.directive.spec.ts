2f1bf7f5a2dfb3e4dbfaba162423ce8f6b9017d1
zaziba.ahmed <ahmedzaziba@gmail.com>
2025-06-28 17:38:51 -0400

    got rid of test error

--- src/app/directives/overdue-highlight.directive.spec.ts
+++ src/app/directives/overdue-highlight.directive.spec.ts
@@ -1,8 +1,14 @@
 import { OverdueHighlightDirective } from './overdue-highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('OverdueHighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new OverdueHighlightDirective();
+    // Mock the dependencies
+    const mockElementRef = {} as ElementRef;
+    const mockRenderer2 = {} as Renderer2;
+
+    // Create the directive instance with mocks
+    const directive = new OverdueHighlightDirective(mockElementRef, mockRenderer2);
     expect(directive).toBeTruthy();
   });
 });
