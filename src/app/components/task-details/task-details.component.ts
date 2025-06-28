5f5ea450f08bf6163399bca0e53833564e79ed11
zaziba.ahmed <ahmedzaziba@gmail.com>
2025-06-28 16:29:21 -0400

    Basic task details and edit form logic implemented

--- src/app/components/task-details/task-details.component.ts
+++ src/app/components/task-details/task-details.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {TaskService} from "../../services/task.service";
+import {MatDialog} from "@angular/material/dialog";
+import {Task} from "../../models/task";
+import {TaskFormComponent} from "../../shared/task-form/task-form.component";
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.scss']
 })
-export class TaskDetailsComponent {
+export class TaskDetailsComponent implements OnInit {
+  task: Task | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+    this.taskService.tasks$.subscribe(tasks => {
+      this.task = tasks.find(t => t.id === id);
+    });
+  }
+
+  openEditDialog() {
+    if (!this.task) return;
+    const dialogRef = this.dialog.open(TaskFormComponent, {
+      width: '500px',
+      data: { task: this.task, isEdit: true }
+    });
+    dialogRef.afterClosed().subscribe((result: Task | undefined) => {
+      if (result) {
+        this.taskService.updateTask(result);
+        // Optionally, you can refresh or update the local task:
+        this.task = result;
+      }
+    });
+  }
 
 }
